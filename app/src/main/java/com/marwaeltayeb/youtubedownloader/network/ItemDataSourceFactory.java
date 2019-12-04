88c8dbb891e368dd530162cf55e062afa0a44541
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-12-05 01:00:07 +0200

    Create ItemDataSourceFactory

---
+++ app/src/main/java/com/marwaeltayeb/youtubedownloader/network/ItemDataSourceFactory.java
@@ -0,0 +1,32 @@
+package com.marwaeltayeb.youtubedownloader.network;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.paging.DataSource;
+import android.arch.paging.PageKeyedDataSource;
+
+import com.marwaeltayeb.youtubedownloader.models.Item;
+
+public class ItemDataSourceFactory extends DataSource.Factory {
+
+    // Creating the mutable live data
+    private MutableLiveData<PageKeyedDataSource<String, Item>> itemLiveDataSource = new MutableLiveData<>();
+
+    @Override
+    public DataSource<String, Item> create() {
+        // Getting our data source object
+        ItemDataSource itemDataSource = new ItemDataSource();
+
+        // Posting the data source to get the values
+        itemLiveDataSource.postValue(itemDataSource);
+
+        // Returning the data source
+        return itemDataSource;
+    }
+
+
+    // Getter for item live data source
+    public MutableLiveData<PageKeyedDataSource<String, Item>> getItemLiveDataSource() {
+        return itemLiveDataSource;
+    }
+
+}
