ad76eff3bdfebbc26e1571be07f2a84656d3d421
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-12-10 23:38:34 +0200

    Show list of download items

--- app/src/main/java/com/marwaeltayeb/youtubedownloader/activities/DownloadActivity.java
+++ app/src/main/java/com/marwaeltayeb/youtubedownloader/activities/DownloadActivity.java
@@ -2,21 +2,37 @@ package com.marwaeltayeb.youtubedownloader.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.Toast;
 
+import com.commit451.youtubeextractor.VideoStream;
+import com.commit451.youtubeextractor.YouTubeExtraction;
+import com.commit451.youtubeextractor.YouTubeExtractor;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 import com.marwaeltayeb.youtubedownloader.R;
 import com.marwaeltayeb.youtubedownloader.YoutubeConfig;
+import com.marwaeltayeb.youtubedownloader.adapter.DownloadAdapter;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class DownloadActivity extends YouTubeBaseActivity {
 
     String idOfVideo;
     YouTubePlayerView youTubePlayerView;
     YouTubePlayer.OnInitializedListener onInitializedListener;
+    RecyclerView recyclerView;
+    DownloadAdapter downloadAdapter;
+    List<VideoStream> videoStreams;
 
     private static final String TAG = "DownloadActivity";
 
@@ -25,12 +41,29 @@ public class DownloadActivity extends YouTubeBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
 
-        youTubePlayerView = findViewById(R.id.youtubePlay);
-
         // Receive the id of teh video
         Intent intent = getIntent();
         idOfVideo = intent.getStringExtra("id");
-        Toast.makeText(this, idOfVideo + "", Toast.LENGTH_SHORT).show();
+
+        playYoutubeVideo();
+
+        setUpRecyclerView();
+
+        showDownloadList();
+    }
+
+    private void setUpRecyclerView(){
+        recyclerView = findViewById(R.id.downloadList);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false).getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    private void playYoutubeVideo(){
+        youTubePlayerView = findViewById(R.id.youtubePlay);
 
         onInitializedListener = new YouTubePlayer.OnInitializedListener() {
             @Override
@@ -45,13 +78,37 @@ public class DownloadActivity extends YouTubeBaseActivity {
             }
         };
 
-
-
         // Play it
         youTubePlayerView.initialize(YoutubeConfig.getApiKey(), onInitializedListener);
-
-
     }
+
+
+    private void showDownloadList(){
+        YouTubeExtractor extractor1 = new YouTubeExtractor.Builder().build();
+        final Disposable v = extractor1.extract(idOfVideo)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<YouTubeExtraction>() {
+                    @Override
+                    public void accept(final YouTubeExtraction youTubeExtraction) throws Exception {
+                        Log.d("Tube", youTubeExtraction + "Done");
+
+                        videoStreams = youTubeExtraction.getVideoStreams();
+                        downloadAdapter = new DownloadAdapter(getBaseContext(),videoStreams);
+                        recyclerView.setAdapter(downloadAdapter);
+
+                        //final String url = String.valueOf(youTubeExtraction.getVideoStreams().get(0).getUrl());
+
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.d("Tube", "Error");
+                    }
+                });
+    }
+
+
 
 
 }
---
+++ app/src/main/java/com/marwaeltayeb/youtubedownloader/adapter/DownloadAdapter.java
@@ -0,0 +1,104 @@
+package com.marwaeltayeb.youtubedownloader.adapter;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.commit451.youtubeextractor.VideoStream;
+import com.marwaeltayeb.youtubedownloader.R;
+
+import java.util.List;
+
+public class DownloadAdapter extends RecyclerView.Adapter<DownloadAdapter.DownloadViewHolder> {
+
+    private Context mContext;
+
+    private List<VideoStream> videoStreamsList;
+
+    private int mItemSelected= -1;
+
+    public DownloadAdapter(Context mContext, List<VideoStream> videoStreamsList) {
+        this.mContext = mContext;
+        this.videoStreamsList = videoStreamsList;
+    }
+
+    @NonNull
+    @Override
+    public DownloadViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.download_item, parent, false);
+        return new DownloadViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DownloadViewHolder holder, int position) {
+        VideoStream currentVideoStream = videoStreamsList.get(position);
+
+        String resolution = currentVideoStream.getResolution() + " - ";
+        String format = currentVideoStream.getFormat().toLowerCase();
+
+        holder.resolutionTextView.setText(resolution);
+        holder.formatTextView.setText(format);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (videoStreamsList == null) {
+            return 0;
+        }
+        return videoStreamsList.size();
+    }
+
+
+    class DownloadViewHolder extends RecyclerView.ViewHolder{
+        // Create view instances
+        TextView resolutionTextView;
+        TextView formatTextView;
+        ImageView downLoadImageView;
+
+        private DownloadViewHolder(View itemView) {
+            super(itemView);
+            resolutionTextView = itemView.findViewById(R.id.resolutionTextView);
+            formatTextView = itemView.findViewById(R.id.formatTextView);
+            downLoadImageView = itemView.findViewById(R.id.img_download);
+
+            downLoadImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Get the position of the view in the adapter
+                    mItemSelected = getAdapterPosition();
+                    notifyDataSetChanged();
+                    String url = videoStreamsList.get(mItemSelected).getUrl();
+                    download(url);
+                    Toast.makeText(mContext, "Download Started", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+
+    }
+
+    private void download(String url){
+        DownloadManager downloadmanager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setTitle("My File");
+        request.setDescription("Downloading");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setVisibleInDownloadsUi(false);
+
+        downloadmanager.enqueue(request);
+    }
+
+
+}
