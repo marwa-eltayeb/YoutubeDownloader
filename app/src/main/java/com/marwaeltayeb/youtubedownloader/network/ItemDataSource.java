0924f98c14dbfde090768062f22ed18cd4668d3c
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-12-11 21:47:46 +0200

    Load next and previous pages

--- app/src/main/java/com/marwaeltayeb/youtubedownloader/network/ItemDataSource.java
+++ app/src/main/java/com/marwaeltayeb/youtubedownloader/network/ItemDataSource.java
@@ -17,47 +17,85 @@ public class ItemDataSource extends PageKeyedDataSource<String, Item> {
 
     private static final String PART = "snippet";
     public static final int MAX_SIZE = 25;
-    private static final String API_KEY = "AIzaSyCLVF4cFbt7ajWqKy4qDPgcW12mcDzEGf4";
 
     @Override
     public void loadInitial(@NonNull LoadInitialParams<String> params, @NonNull final LoadInitialCallback<String, Item> callback) {
 
         RetrofitClient.getInstance()
-                .getYoutubeService().getVideos(PART,MAX_SIZE, PlaylistActivity.keyWord, YoutubeConfig.getApiKey())
+                .getYoutubeService().getVideos(PART, MAX_SIZE, PlaylistActivity.keyWord, YoutubeConfig.getApiKey())
                 .enqueue(new Callback<YoutubeApiResponse>() {
                     @Override
                     public void onResponse(Call<YoutubeApiResponse> call, Response<YoutubeApiResponse> response) {
                         YoutubeApiResponse youtubeApiResponse = response.body();
 
-                        if (response.body() != null) {
+                        if (youtubeApiResponse != null) {
                             // Fetch data and pass the result  null for the previous page
-                            callback.onResult(response.body().getItems(), null, response.body().getNextPageToken());
+                            callback.onResult(youtubeApiResponse.getItems(), null, youtubeApiResponse.getNextPageToken());
                         }
 
-                        if(youtubeApiResponse.getItems() != null){
-                            Log.d("SizeOfVideos",youtubeApiResponse.getItems().size() + " ");
+                        if (youtubeApiResponse.getItems() != null) {
+                            Log.d("SizeOfVideos", youtubeApiResponse.getItems().size() + " ");
                         }
 
                         if (!response.isSuccessful()) {
-                            Log.d("Result","Code: " + response.code());
+                            Log.d("Result", "Code: " + response.code());
                         }
                     }
 
                     @Override
                     public void onFailure(Call<YoutubeApiResponse> call, Throwable t) {
-                        Log.d("onFailure","Failed");
+                        Log.d("onFailure", "Failed");
                     }
                 });
     }
 
     @Override
-    public void loadBefore(@NonNull LoadParams<String> params, @NonNull LoadCallback<String, Item> callback) {
+    public void loadBefore(@NonNull final LoadParams<String> params, @NonNull final LoadCallback<String, Item> callback) {
+        RetrofitClient.getInstance()
+                .getYoutubeService().getVideos(PART, MAX_SIZE, PlaylistActivity.keyWord, YoutubeConfig.getApiKey())
+                .enqueue(new Callback<YoutubeApiResponse>() {
+                    @Override
+                    public void onResponse(Call<YoutubeApiResponse> call, Response<YoutubeApiResponse> response) {
+                        YoutubeApiResponse youtubeApiResponse = response.body();
+
+                        String key = youtubeApiResponse.getPrevPageToken() != null ? params.key : null;
+                        if (response.body() != null) {
+                            // Passing the loaded database and the previous page key
+                            callback.onResult(youtubeApiResponse.getItems(), key);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<YoutubeApiResponse> call, Throwable t) {
+                        Log.d("onFailure", "Failed");
+                    }
+                });
 
     }
 
     @Override
-    public void loadAfter(@NonNull LoadParams<String> params, @NonNull LoadCallback<String, Item> callback) {
+    public void loadAfter(@NonNull final LoadParams<String> params, @NonNull final LoadCallback<String, Item> callback) {
+        RetrofitClient.getInstance()
+                .getYoutubeService().getVideos(PART, MAX_SIZE, PlaylistActivity.keyWord, YoutubeConfig.getApiKey())
+                .enqueue(new Callback<YoutubeApiResponse>() {
+                    @Override
+                    public void onResponse(Call<YoutubeApiResponse> call, Response<YoutubeApiResponse> response) {
+                        YoutubeApiResponse youtubeApiResponse = response.body();
 
+                        if (youtubeApiResponse != null) {
+                            // If the response has next page, load it
+                            String key = youtubeApiResponse.getNextPageToken() != null ? params.key : null;
+
+                            // Passing the loaded database and next page value
+                            callback.onResult(youtubeApiResponse.getItems(), key);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<YoutubeApiResponse> call, Throwable t) {
+                        Log.d("onFailure", "Failed");
+                    }
+                });
     }
 
 }
