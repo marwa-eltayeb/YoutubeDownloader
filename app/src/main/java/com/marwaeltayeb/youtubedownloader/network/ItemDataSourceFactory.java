682c5c93d894581c16e913fee5c349480d933855
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-12-19 05:30:26 +0200

    Migrate to Androidx

--- app/src/main/java/com/marwaeltayeb/youtubedownloader/network/ItemDataSourceFactory.java
+++ app/src/main/java/com/marwaeltayeb/youtubedownloader/network/ItemDataSourceFactory.java
@@ -1,8 +1,8 @@
 package com.marwaeltayeb.youtubedownloader.network;
 
-import android.arch.lifecycle.MutableLiveData;
-import android.arch.paging.DataSource;
-import android.arch.paging.PageKeyedDataSource;
+import androidx.lifecycle.MutableLiveData;
+import androidx.paging.DataSource;
+import androidx.paging.PageKeyedDataSource;
 
 import com.marwaeltayeb.youtubedownloader.models.Item;
 
