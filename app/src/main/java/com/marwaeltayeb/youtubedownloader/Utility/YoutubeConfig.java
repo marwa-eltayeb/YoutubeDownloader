3c5c76c45741cb1fa64dd1883ea8d0ab89055144
Marwa-Said <marwa_eltayeb@yahoo.com>
2021-01-26 00:36:26 +0200

    Hide API Key

--- app/src/main/java/com/marwaeltayeb/youtubedownloader/Utility/YoutubeConfig.java
+++ app/src/main/java/com/marwaeltayeb/youtubedownloader/Utility/YoutubeConfig.java
@@ -1,11 +1,13 @@
 package com.marwaeltayeb.youtubedownloader.Utility;
 
+import com.marwaeltayeb.youtubedownloader.BuildConfig;
+
 public class YoutubeConfig {
 
     public YoutubeConfig() {
     }
 
-    private static final String API_KEY = "AIzaSyDzOz6qKlbHhKipDK-CRx-C4tdRg9CxJrs";
+    private static final String API_KEY = BuildConfig.API_KEY;
 
     public static String getApiKey() {
         return API_KEY;
